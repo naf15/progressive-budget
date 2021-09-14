646cddeefa72269fbfb3c3baebec62c6da435dfb
Nafis Rahman <nmr278@nyu.edu>
2021-09-14 19:48:36 -0400

    finishes checkDatabase function

--- public/db.js
+++ public/db.js
@@ -31,10 +31,33 @@ function saveRecord(record) {
     budgetStore.add(record); 
   }
   
-  function checkDatabase() {
-    // open a transaction on your pending db
-    // access your pending object store
-    // get all records from store and set to a variable
-    const transaction = db.transaction(['BudgetStore'], 'readwrite');
-    const budgetStore = transaction.objectStore('BudgetStore');
-    const getAll = budgetStore.getAll();
+function checkDatabase() {
+// open a transaction on your pending db
+// access your pending object store
+// get all records from store and set to a variable
+const transaction = db.transaction(['BudgetStore'], 'readwrite');
+const budgetStore = transaction.objectStore('BudgetStore');
+const getAll = budgetStore.getAll();
+
+getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+        fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+        },
+        })
+        .then((response) => response.json())
+        .then(() => {
+            // if successful, open a transaction on your pending db
+            // access your pending object store
+            // clear all items in your store
+            const transaction = db.transaction(['BudgetStore'], 'readwrite');
+            const budgetStore = transaction.objectStore('BudgetStore');
+            budgetStore.clear()
+        });
+    }
+    };
+}
