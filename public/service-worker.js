a75b15f4e029349be4f89c7d601010a1d7613834
Nafis Rahman <nmr278@nyu.edu>
2021-09-14 20:12:28 -0400

    Updated eventListener to unclude /api/transaction route

--- public/service-worker.js
+++ public/service-worker.js
@@ -21,6 +21,17 @@ const FILES_TO_CACHE = [
         return cache.addAll(FILES_TO_CACHE);
       })
     );
+
+       // evt.waitUntil(
+    //     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    // );
+
+    evt.waitUntil(
+        caches.open(DATA_CACHE_NAME).then((cache) => {
+            return cache.add('/api/transaction')
+        })
+    );
+ 
   
     self.skipWaiting();
   });
